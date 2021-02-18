cd40c96bef3b0fd03bbef76eb0667fb17225926f
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-18 13:19:38 -0800

    added css classes to handle rotation of car. #2

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var $hotWheel = document.querySelector('#car');
 
 var carData ={
-  facing: 'right';
+  facing: 'right',
 }
 
 function carController(event){
-  console.log('event', event )
-  console.log('event.target', event.target )
+  console.log('event', event );
+  console.log('event.target', event.target );
 }
 
 window.addEventListener('keydown', carController);
