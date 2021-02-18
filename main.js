73c616baaca0da3149274bf0a514d3cde5b11375
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-18 13:08:17 -0800

    created data model that records what direction car is facing #2

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var $hotWheel = document.querySelector('#car');
 
+var carData ={
+  facing: 'right';
+}
+
 function carController(event){
   console.log('event', event )
   console.log('event.target', event.target )
