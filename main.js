8369e6f8090f6c671d730326c8fe7560a5ae7c97
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-18 15:12:51 -0800

    polished turning functionality and removed unnecessary css rules. #2

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ function carController(event) {
   } else if (event.keyCode === 38) {
     $hotWheel.className = 'rotate-90-left';
     carData.facing = 'north';
+  } else if (event.keyCode === 39) {
+    $hotWheel.className = '';
+    carData.facing = 'east';
+  } else if (event.keyCode === 40) {
+    $hotWheel.className = 'rotate-90-right';
+    carData.facing = 'south';
   }
 }
 
